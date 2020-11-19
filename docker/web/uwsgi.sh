33b6946940747d35d119f27e21670a54981e9d9a
Jonathan Green <jonathan@razzard.com>
2020-11-18 22:46:52 -0400

    Building

--- docker/web/uwsgi.sh
+++ docker/web/uwsgi.sh
@@ -9,12 +9,12 @@ chown -R simplified:simplified /var/log/uwsgi
 
 # Defer uwsgi service to simplified.
 mkdir /etc/service/runsvdir-simplified
-cp /ls_build/services/simplified_user.runit /etc/service/runsvdir-simplified/run
+cp /ls_build/uwsgi/simplified_user.runit /etc/service/runsvdir-simplified/run
 
 # Prepare uwsgi for runit.
 app_home=/home/simplified
 mkdir -p $app_home/service/uwsgi
-cp /ls_build/services/uwsgi.runit $app_home/service/uwsgi/run
+cp /ls_build/uwsgi/uwsgi.runit $app_home/service/uwsgi/run
 chown -R simplified:simplified $app_home/service
 
 # Create an alias to restart the application.
